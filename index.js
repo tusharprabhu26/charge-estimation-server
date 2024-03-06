8c1534ab7bec8f3bf5efbb4392735e9d26d7004f
Tushar Prabhu <tusharprabhu26@gmail.com>
2024-03-06 12:35:26 +0530

    feat: add server setup

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('This is the starting point');
+const express = require('express');
+const routes = require('./chargeTimeRoutes');
+
+const app = express();
+app.use(express.json());
+
+app.use('/estimate-charging-time', routes);
+
+const port = 3001;
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+module.exports = app;
